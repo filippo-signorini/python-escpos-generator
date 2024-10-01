10dcd5b0d830ddaed87c0b2773ff4061068d8c1c
Filippo Signorini <f.signorini@elsyco.it>
2024-10-01 16:38:14 +0200

    Corretto errore in generazione barcode

--- src/escpos_generator/__init__.py
+++ src/escpos_generator/__init__.py
@@ -1,11 +1,10 @@
-from .codes import Barcode, BarcodeType, QRCode, QRCorrection, QRSize
+from .codes import Barcode, QRCode, QRCorrection, QRSize
 from .enums import PosPaperSize
 from .generator import Generator
 from .style import PosAlign, PosFontType, PosRotation, PosStyle, PosTextSize
 
 __all__ = [
     "Barcode",
-    "BarcodeType",
     "QRCode",
     "QRCorrection",
     "QRSize",
