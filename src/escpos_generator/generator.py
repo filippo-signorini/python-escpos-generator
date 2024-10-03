b9b1b09394d1d3c56e6878e6d042a4f26d723a34
Filippo Signorini <f.signorini@elsyco.it>
2024-10-03 16:19:59 +0200

    Aggiunto controllo connessione stampante

--- src/escpos_generator/generator.py
+++ src/escpos_generator/generator.py
@@ -1,19 +1,59 @@
+import logging
+import time
 from math import floor
 from typing import Optional
 
+from escpos.exceptions import DeviceNotFoundError, USBNotFoundError
 from escpos.printer import Usb
 from typing_extensions import assert_never
+from usb import USBError
 
 from .codes import Barcode, QRCode
 from .enums import PosPaperSize
 from .style import PosAlign, PosFontType, PosStyle
 
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+
 
 class Generator:
-    def __init__(self, idVendor: int, idProduct: int, paper_size: PosPaperSize) -> None:
-        self.p = Usb(idVendor, idProduct)
+    def __init__(
+        self,
+        idVendor: int,
+        idProduct: int,
+        paper_size: PosPaperSize,
+        debug=False,
+    ) -> None:
+        self._id_vendor = idVendor
+        self._id_product = idProduct
         self.paper_size = paper_size
         self._currentStyle = PosStyle.defaults()
+        self.p = Usb(self._id_vendor, self._id_product)
+
+    def check_usb(self) -> bool:
+        try:
+            self.p.buzzer()
+        except (USBNotFoundError, DeviceNotFoundError, USBError):
+            logger.error("Stampante non trovata, tento riconnessione")
+            conn = False
+            max_tries = 2
+            for i in range(max_tries):
+                try:
+                    self.p.open()
+                except (USBNotFoundError, DeviceNotFoundError):
+                    if i + 1 < max_tries:
+                        time.sleep(5)
+                else:
+                    conn = True
+                    break
+
+            if conn:
+                logger.info("Stampante riconnessa")
+            else:
+                logger.warning("Impossibile connettersi alla stampante, salto")
+            return conn
+        else:
+            return True
 
     @property
     def max_chars_per_line(self) -> int:
@@ -25,10 +65,16 @@ class Generator:
         assert_never(self.paper_size)
 
     def init(self) -> None:
+        if not self.check_usb():
+            return
+
         self.p.hw("INIT")
         self._set_style(self._currentStyle)
 
     def _set_style(self, style: PosStyle) -> None:
+        if not self.check_usb():
+            return
+
         self.p.set(
             align=style.align.name if style.align is not None else None,
             bold=style.bold,
@@ -52,6 +98,9 @@ class Generator:
         style: Optional[PosStyle] = None,
         keep_style: bool = False,
     ) -> None:
+        if not self.check_usb():
+            return
+
         if style is not None:
             self._set_style(style.merge(self._currentStyle, allow_null=True))
             if keep_style:
@@ -64,23 +113,38 @@ class Generator:
             self._set_style(self._currentStyle.merge(style, allow_null=True))
 
     def cut(self) -> None:
-        self.p.cut()
+        if not self.check_usb():
+            return
+
+        self.p.cut(feed=False)
 
     def empty_lines(self, lines: int = 1) -> None:
+        if not self.check_usb():
+            return
+
         if lines > 0:
             self.p.ln(lines)
 
     def hr(self, half: bool = False) -> None:
+        if not self.check_usb():
+            return
+
         line = "-" * floor(
             self.max_chars_per_line / 2 if half else self.max_chars_per_line
         )
         self.text(line, style=PosStyle(align=PosAlign.center))
 
     def set_style(self, style: PosStyle) -> None:
+        if not self.check_usb():
+            return
+
         self._set_style(style.merge(self._currentStyle, allow_null=True))
         self._currentStyle = style.merge(self._currentStyle, allow_null=False)
 
     def qr(self, qr_code: QRCode) -> None:
+        if not self.check_usb():
+            return
+
         self.p.qr(
             qr_code.data,
             ec=qr_code.correction.value,
@@ -89,6 +153,9 @@ class Generator:
         )
 
     def barcode(self, barcode: Barcode) -> None:
+        if not self.check_usb():
+            return
+
         self.p.barcode(
             barcode.data,
             bc=barcode.type.name,
@@ -99,11 +166,17 @@ class Generator:
         )
 
     def image(self, image: str, align: PosAlign = PosAlign.center) -> None:
+        if not self.check_usb():
+            return
+
         self._set_style(PosStyle(align=align))
         self.p.image(image)
         self._set_style(self._currentStyle)
 
     def test_page(self):
+        if not self.check_usb():
+            return
+
         self.empty_lines()
         self.text("   0 1 2 3 4 5 6 7 8 9 A B C D E F ")
         self.empty_lines()
